5d9ea5d5dd95ee18ea0f9a0ea9dcf43169ae104a
Andrey_Fominykh <meriadok2003@mail.ru>
2022-04-06 20:07:17 +0300

    add files by first step

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,11 @@
-import requests
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+class IndexPageView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
